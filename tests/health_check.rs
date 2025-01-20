f81ebdfc45399f61aefd5fcc5c626c25748b9000
zephoon <zephoonyu@protonmail.com>
2025-01-21 07:24:25 +0800

    fix: fix the test case never completion

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,6 @@
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app().await.expect("Failed to spawn our app.");
+    spawn_app();
     let client = reqwest::Client::new();
     let response = client
         .get("http://127.0.0.1:8000/health_check")
@@ -11,6 +11,7 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-async fn spawn_app() -> Result<(), std::io::Error> {
-    zero2prod::run().await
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+    let _ = tokio::spawn(server);
 }
