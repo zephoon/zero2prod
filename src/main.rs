104c10394d81b882c30dc1336d392015e493c187
zephoon <zephoonyu@protonmail.com>
2025-01-21 22:35:17 +0800

    chore: enable unit test case support random port

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    print!("http://127.0.0.1:{}", port);
+    run(listener)?.await
 }
